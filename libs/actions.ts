9d5796d0362b992eb2dbe10e8f4c854698834a97
muhdshanid <shanishanid91@gmail.com>
2023-07-02 23:14:58 +0530

    action functions completed

--- libs/actions.ts
+++ libs/actions.ts
@@ -29,15 +29,120 @@ export const getCurrentUser = async () => {
     }
 }
 
-export const getListings = async () => {
+export const getListings = async (params: { userId?: string}) => {
     
+
     try {
-        
-        const listings = await prisma.listing.findMany({orderBy: {
+        const {userId} = params
+
+        let query: any = {}
+
+        if(userId){
+            query.userId = userId
+        }
+        const listings = await prisma.listing.findMany({
+            where: query,
+            orderBy: {
             createdAt: "desc"
         }})
 
-        return listings
+        const safeListings = listings.map((listing) => ({
+            ...listing,
+            createdAt: listing.createdAt.toISOString()
+        }))
+        return safeListings
+    } catch (error: any) {
+        throw new Error(error)
+    }
+}
+
+export const getListingById = async (params: {listingId?: string}) =>{
+
+    try {
+        const {listingId} = params
+
+        const listing = await prisma.listing.findUnique({
+            where: { id: listingId},
+            include: {
+                user: true
+            }
+        })
+
+        if(!listing) return null
+
+        return {
+            ...listing,
+            createdAt: listing.createdAt.toISOString(),
+            user: {
+                ...listing.user, 
+                createdAt: listing.user.createdAt.toISOString(),
+                updatedAt: listing.user.updatedAt.toISOString(),
+                emailVerified: listing.user.emailVerified?.toISOString() || null
+            }
+        }
+    } catch (error: any) {
+        throw new Error(error)
+    }
+}
+
+export const getReservations = async (params: {listingId?: string, authorId?: string, userId?: string}) => {
+
+    try {
+        
+    
+    const { listingId, authorId, userId} = params
+
+    const query: any = {}
+
+    if(listingId) {
+        query.listingId = listingId
+    }
+    if(userId) {
+        query.userId = userId
+    }
+    if(authorId) {
+        query.listing = { userId: authorId}
+    }
+
+    const reservations = await prisma.reservation.findMany({where: query, include: {
+        listing: true
+    }, orderBy: {
+        createdAt: "desc"
+    }})
+
+    const safeReservations = reservations.map(reservation => ({
+        ...reservation,
+        createdAt: reservation.createdAt.toISOString(),
+        startDate: reservation.startDate.toISOString(),
+        endDate: reservation.endDate.toISOString(),
+        listing: {
+            ...reservation.listing,
+            createdAt: reservation.listing.createdAt.toISOString()
+        }
+    }))
+    return safeReservations
+} catch (error: any) {
+        throw new Error(error)
+}
+}
+
+export const getFavoriteListings = async () => {
+
+    try {
+        const currentUser = await getCurrentUser()
+
+        if(!currentUser) return []
+
+        const favorites = await prisma.listing.findMany({where: {id: {
+            in: [...(currentUser.favoriteIds || [])]
+        }}})
+
+        const safeFavorites = favorites.map(fav => ({
+            ...fav,
+            createdAt: fav.createdAt.toISOString()
+        }))
+
+        return safeFavorites
     } catch (error: any) {
         throw new Error(error)
     }
