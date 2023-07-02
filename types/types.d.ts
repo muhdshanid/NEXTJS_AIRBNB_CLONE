b9322da835ba7a5195bd8b2d05ef7a8cdf08dbca
muhdshanid <shanishanid91@gmail.com>
2023-07-02 15:43:41 +0530

    common types for app

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,3 +1,4 @@
+import { Listing, Reservation, User } from "@prisma/client"
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form"
 import { IconType } from "react-icons"
 
@@ -59,4 +60,107 @@ type RegisterModalStore = {
     onOpen: () => void,
     onClose: () => void,
 }
+type LoginModalStore = {
+    isOpen: boolean,
+    onOpen: () => void,
+    onClose: () => void,
+}
+type RentModalStore = {
+    isOpen: boolean,
+    onOpen: () => void,
+    onClose: () => void,
+}
 
+
+
+type SafeUser = Omit<
+User,
+"createdAt" | "updatedAt" | "emailVerified"> & {
+    createdAt: string,
+    updatedAt: string,
+    emailVerified: string | null,
+}
+
+type NavbarProps = {
+    currentUser?: SafeUser | null
+}
+
+type UserMenuProps = {
+    currentUser?: SafeUser | null
+}
+
+
+type AvatarProps = {
+    src?: string | null | undefined
+}
+
+type CategoryBoxProps = {
+    label: string,
+    icon: IconType,
+    selected?: boolean
+    
+}
+
+type CategoryInputProps = {
+    icon: IconType,
+    onClick: (value: string) => void
+    label: string,
+    selected?: boolean
+}
+
+type CountrySelectValue = {
+    flag: string,
+    label: string,
+    latlng: number[],
+    region: string,
+    value: string
+}
+
+type CountrySelectProps = {
+    value?: CountrySelectValue,
+    onChange: (value: CountrySelectValue) => void
+}
+
+type MapProps = {
+    center?: number[]
+}
+
+type CounterProps = {
+    title: string,
+    subtitle: string,
+    value: number,
+    onChange: (value: number) => void
+}
+
+type ImageUploadProps = {
+    onChange: (value: string) => void,
+    value: string
+}
+
+type EmptyStateProps = {
+    title?: string,
+    subtitle?: string,
+    showReset?: boolean
+}
+
+
+type ListingCardProps = {
+    listing: Listing,
+    reservation?: Reservation,
+    onAction?: (id: string) => void,
+    disabled?: boolean,
+    actionLabel?: string,
+    actionId?: string,
+    currentUser?: SafeUser | null
+}
+
+
+type HeartButtonProps = {
+    listingId: string,
+    currentUser?: SafeUser | null
+}
+
+type UseFavorite = {
+    listingId: string,
+    currentUser?: SafeUser | null
+}
