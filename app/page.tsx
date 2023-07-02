17b7a7ece9903bd14889125ce1f9034e64a1e867
muhdshanid <shanishanid91@gmail.com>
2023-07-02 23:14:10 +0530

    added params to get listings

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,14 @@ import Container from '@/components/Container'
 import EmptyState from '@/components/EmptyState'
 import ListingCard from '@/components/listings/ListingCard'
 import { getCurrentUser, getListings } from '@/libs/actions'
+import { IListingParams } from '@/types/types'
 
-export default async function Home() {
-  const listings = await getListings()
+
+interface HomeProps {
+  searchParams: IListingParams
+}
+export const Home = async ({searchParams}: HomeProps) => {
+  const listings = await getListings(searchParams)
   const currentUser = await getCurrentUser()
   if(listings.length === 0){
     return (
@@ -16,7 +21,7 @@ export default async function Home() {
       <div className='pt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3
       lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
         {
-          listings.map((listing: any) => {
+          listings.map((listing) => {
             return (
               <ListingCard
               currentUser={currentUser}  key={listing.id} listing={listing}/>
